ab2014db755065e90193ff824803c5131d48af03
Zabih <mdzabihhaqqani@gmail.com>
2021-11-23 14:49:41 +0000

    added chalk library

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 let readlineSync = require("readline-sync");
 
+const chalk = require('chalk');
+
 //Taking input from the user//
 
-let userName = readlineSync.question("Hi what's your name ? ")
+
+let userName = readlineSync.question("Hi what's your name ?\n")
 
 console.log("Welcome", userName, "to the game on how well do you know about IPL")
 
 let score = 0;
 
+// processing the data //
 function questions(question, answer) {
 
   let userAnswer = readlineSync.question(question)
 
   if (userAnswer.toUpperCase() === answer.toUpperCase()) {
-    console.log("Right Answer")
+    console.log(chalk.green("Right Answer"));
     score = score + 10;
   } else {
-    console.log("Wrong Answer")
+    console.log(chalk.red("Wrong Answer"));
   }
   console.log("current score =", score)
   console.log("---------------------")
 }
 
 let allQuestions = [{
-  question: "Full Form of IPL ? ",
+  question: "Full Form of IPL ?\n",
   answer: "Indian Premier League"
 
 }, {
-  question: "Who owns Kolkata Knight Riders ? ",
+  question: "Who owns Kolkata Knight Riders ?\n",
   answer: ("ShahRukhKhan")
 }, {
-  question: "Which team has won the most IPL Matches ? ",
+  question: "Which team has won the most IPL Matches ?\n",
   answer: "Mumbai Indians"
 }
 
 ]
 
+// giving the output //
 
 for (let i = 0; i < allQuestions.length; i++) {
   let currentquestions = allQuestions[i];
@@ -43,9 +48,9 @@ for (let i = 0; i < allQuestions.length; i++) {
 
 
 }
-console.log("final score = ", score)
-if (score === 3) {
+console.log(chalk.blue("final score = ", score));
+if (score === 30) {
 
-  console.log("YAY you got all of them right")
+  console.log(chalk.blue("YAY you got all of them right"));
 
-} 
+}
